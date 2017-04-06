513393508abf369f187556585867569907b98436
BDXletty <laetitiadervaux33@gmail.com>
2017-04-06 10:29:46 +0200

    validation mot de passe pour creation compte utilisateur

--- frontend/app/components/users/components.users.js
+++ frontend/app/components/users/components.users.js
@@ -5,43 +5,56 @@
  */
 COMPNT.component('userCreate', {
 
-        templateUrl: '/app/components/users/signup.html',
+    templateUrl: '/app/components/users/signup.html',
 
-        bindings: {
-            user: '<'
-        },
+    bindings: {
+        user: '<'
+    },
 
-        controller: ['usersService',
+    controller: ['usersService',
 
-            function (usersService) {
+        function (usersService) {
+            var self = this;
+            
+            var userAdd = {};
+            this.verif = (user) => {
+                if (user.password != user.passwordConfirm) {
 
-                // Save the new user
-                this.addUser = (user) => {
-                    usersService.addUser(user).then(() => {
-                        //$state.go('index');
-                        console.log('yEEESSSS!');
-                    }).catch((err) => { });
-                };
+                    self.message = "Tes mots de passe sont différents";
+                    console.log("mot de pass erroné");
+                    this.userPassword="";
+                } else {          // Save the new user
+                    userAdd = {
+                        'email' : user.email,
+                        'password' : user.password
+                    }
+                        usersService.addUser(userAdd).then(() => {
+                            //$state.go('index');
+                            console.log('yEEESSSS!');
+                        }).catch((err) => { });
+                    
+                }
             }
-        ]
+        }
+    ]
 
-        /*this.user = {};
-        this.user['email'] = $ctrl.user.email;
-        this.user['password'] = $ctrl.user.password;
-        this.message = "";/*
+    /*this.user = {};
+    this.user['email'] = $ctrl.user.email;
+    this.user['password'] = $ctrl.user.password;
+    this.message = "";/*
  
-     
  
-            if (!$ctrl.user.email.indexOf('@') != -1 && !$ctrl.user.email.indexOf('.') != -1) {
-                this.message = "Saisis une adresse mail correcte, stp";
-            }
-            if ($ctrl.user.email == "" || $ctrl.user.password == "" || $ctrl.user.passwordVerif == "") {
-                this.message = "Remplis tous les champs stp";
-            }
-            if ($ctrl.user.password != $ctrl.user.passwordVerif) {
  
-                this.message = "Tes mots de passe sont différents";
-            }
-            else {*/
-        // $ctrl.user ='';
-    });
+        if (!$ctrl.user.email.indexOf('@') != -1 && !$ctrl.user.email.indexOf('.') != -1) {
+            this.message = "Saisis une adresse mail correcte, stp";
+        }
+        if ($ctrl.user.email == "" || $ctrl.user.password == "" || $ctrl.user.passwordVerif == "") {
+            this.message = "Remplis tous les champs stp";
+        }
+        if ($ctrl.user.password != $ctrl.user.passwordVerif) {
+ 
+            this.message = "Tes mots de passe sont différents";
+        }
+        else {*/
+    // $ctrl.user ='';
+});
