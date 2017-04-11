7e292dda5a9438216eeabf2e8d183fca7e6165b4
Sacha GRASSO <gr.sacha.glgm@gmail.com>
2017-04-11 10:43:38 +0200

    modifs routes et ajout liens internes questions et users

--- frontend/app/components/questions_list/questions_list.component.js
+++ frontend/app/components/questions_list/questions_list.component.js
@@ -13,24 +13,4 @@ COMPNT
       query: '@',
       questions: '<'
     },
-
-    controller: ['QuestionsService', function (QuestionsService) {
-
-      this.model = {
-        list: [],
-        query: ''
-      };
-
-      this.$onInit = () => {
-        this.getAllItems();
-      };
-
-      this.getAllItems = () => {
-        QuestionsService.getQuestions().then((items) => {
-          this.model.list = items.data;
-          console.log(items.data);
-        }).catch((err) => {});
-      };
-
-    }]
   });
---
+++ frontend/app/components/user_profile/user_profile.component.js
@@ -0,0 +1,13 @@
+'use strict';
+
+
+COMPNT
+    .component('userProfile', {
+        templateUrl: '/app/components/user_profile/user_profile.html',
+
+        bindings: {
+            person: '<'
+        }
+
+
+    });
