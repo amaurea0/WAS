9329d70064068474506e418a1829b8c833aec643
Piotr-V <pierrevicq@hotmail.fr>
2017-04-12 07:31:58 +0200

    Rétablissement Nuage

--- frontend/app/components/nuage/nuage.component.js
+++ frontend/app/components/nuage/nuage.component.js
@@ -2,9 +2,27 @@
 
 COMPNT.component('nuage', {
 
-  templateUrl: '/app/components/nuage/nuage.html', 
+  templateUrl: '/app/components/nuage/nuage.html',
 
   bindings: {
-  }
+    tags: '<'
+  },
 
+  controller: ['TagsService', function (TagsService) {
+
+    this.$onInit = () => {
+      this.tags = [];
+
+      this.getAllItems();
+    };
+
+    this.getAllItems = () => {
+      TagsService.getTags().then((items) => {
+        this.tags = items;
+        console.log(items);
+
+      }).catch((err) => { });
+    };
+
+  }]
 });
