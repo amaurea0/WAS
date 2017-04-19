c67c49a5a603524905872c13a31770b07e70a79d
Amaurea <aurore.chatagnier@gmail.com>
2017-04-19 16:13:36 +0200

    regler derniers conflits

--- frontend/app/app.routes.js
+++ frontend/app/app.routes.js
@@ -71,10 +71,12 @@ WEA.config(function ($stateProvider) {
                     var id_question = $transition$.params().idQuestion;
                     QuestionsService.getSpecificQuestion(id_question).then((resp) => {
                         nb_view = resp.nb_views + 1;
-                        QuestionsService.viewQuestion(id_question, {"nb_views": nb_view}).then((rep) => {
-                            return QuestionsService.getSpecificQuestion(id_question);
-                        })
+                        QuestionsService.viewQuestion(id_question, {
+                            "nb_views": nb_view
+                        }).then((rep) => {})
                     }).catch((err) => {});
+
+                    return QuestionsService.getSpecificQuestion(id_question);
                 }
             }
         })
