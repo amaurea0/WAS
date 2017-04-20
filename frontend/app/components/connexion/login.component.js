5d0c526533eec23cdabc9db5bf0380c289c009ee
Amaurea <aurore.chatagnier@gmail.com>
2017-04-20 13:56:06 +0200

    ajout de tous les champs utiles lors de la création d'un utilisateur

--- frontend/app/components/connexion/login.component.js
+++ frontend/app/components/connexion/login.component.js
@@ -5,35 +5,52 @@ WEA.component('login', {
     templateUrl: '/frontend/app/components/connexion/login.html',
 
     bindings: {
-      redirect: '<'
+        redirect: '<'
     },
 
     controller: function (AuthService, $state) {
 
-      this.$onInit = () => {
-        this.errorMessage = '';
-        this.user = {};
-      };
+        this.$onInit = () => {
+            this.errorMessage = '';
+            this.user = {};
+        };
 
-      this.submit = (user) => {
-          console.log(user)
+        this.submitConnexion = (user) => {
+            console.log(user)
 
-        this.errorMessage = '';
+            this.errorMessage = '';
 
-        if (typeof user.name != 'undefined') {
-          AuthService.createUser(user).then(() => {
-            $state.go(this.redirect ? this.redirect : 'questions');
-          }).catch(() => {
-            this.errorMessage = `Une erreur s'est produite`;
-          });
-        } else {
-          AuthService.connect(user.email, user.password).then(() => {
-            console.log(this.redirect)
-            $state.go(this.redirect ? this.redirect : 'questions');
-          }).catch(() => {
-            this.errorMessage = `Utilisateur introuvable ou mot de passe invalide`;
-          });
-        }
-      };
+            AuthService.connect(user.email, user.password).then(() => {
+                console.log(this.redirect)
+                $state.go(this.redirect ? this.redirect : 'questions');
+            }).catch(() => {
+                this.errorMessage = `Utilisateur introuvable ou mot de passe invalide`;
+            });
+
+        };
+
+        this.submitCreation = (user) => {
+            console.log(user)
+
+            this.errorMessage = '';
+
+            var new_user = {
+                "email": user.email,
+                "pass": user.password,
+                "last_name": "",
+                "first_name": "",
+                "pseudo": user.pseudo,
+                "date": new Date(),
+                "avatar": "",
+                "profile": ""
+            }
+
+            AuthService.createUser(new_user).then(() => {
+                $state.go(this.redirect ? this.redirect : 'questions');
+            }).catch(() => {
+                this.errorMessage = `Une erreur s'est produite`;
+            });
+
+        };
     }
-  });
+});
