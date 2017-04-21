55bc8540ca077f301d456516c57dd928de0783b1
Amaurea <aurore.chatagnier@gmail.com>
2017-04-21 11:12:06 +0200

    remove satellizer

--- frontend/app/app.module.js
+++ frontend/app/app.module.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var WEA = angular.module('WildExchange', ['wild.components', 'wild.services', 'ui.router', 'ui.materialize', 'satellizer']);
+var WEA = angular.module('WildExchange', ['wild.components', 'wild.services', 'ui.router', 'ui.materialize']);
 
 const COMPNT = angular.module('wild.components', []);
 
