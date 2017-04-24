8163b87048075f81c5afb1f7ca3b94c30c8dfee9
Sacha GRASSO <gr.sacha.glgm@gmail.com>
2017-04-24 18:04:34 +0200

    commit du lundi

--- frontend/app/app.module.js
+++ frontend/app/app.module.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var WEA = angular.module('WildExchange', ['wild.components', 'wild.services', 'ui.router', 'ui.materialize', 'angularUtils.directives.dirPagination']);
+var WEA = angular.module('WildExchange', ['wild.components', 'wild.services', 'ui.router', 'ui.materialize', 'angularUtils.directives.dirPagination', '720kb.tooltips', 'angular-notification-icons', 'angularTrix']);
 
 const COMPNT = angular.module('wild.components', []);
 
